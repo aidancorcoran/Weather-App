a6e4e940324da7d9bda81acc224eefd2bab540d9
aidancorcoran <ajcorcoran2001@gmail.com>
2023-08-14 17:04:30 -0400

    creating my own framework to work with the api

--- javascript/api.js
+++ javascript/api.js
@@ -1,31 +1,45 @@
-const { call } = require('function-bind');
-var WeatherApi = require('weather_api');
-var defaultClient = WeatherApi.ApiClient.instance;
+// Get apiKey data
+const fs = require('fs');
+const filePath = '../api_key.txt'; // Replace with the actual file path
 
-// Configure API key authorization: ApiKeyAuth
-var ApiKeyAuth = defaultClient.authentications['ApiKeyAuth'];
-ApiKeyAuth.apiKey = '';
-// Uncomment the following line to set a prefix for the API key, e.g. "Token" (defaults to null)
-//ApiKeyAuth.apiKeyPrefix = 'Token';
-
-var apiInstance = new WeatherApi.APIsApi();
-
-var q = "London"; // String | Pass US Zipcode, UK Postcode, Canada Postalcode, IP address, Latitude/Longitude (decimal degree) or city name. Visit [request parameter section](https://www.weatherapi.com/docs/#intro-request) to learn more.
-
-var days = 7; // Number | Number of days of weather forecast. Value ranges from 1 to 14
-
-var opts = { 
-  'dt': new Date("2023-08-15"), // Date | Date should be between today and next 14 day in yyyy-MM-dd format. e.g. '2015-01-01'
-  'unixdt': 56, // Number | Please either pass 'dt' or 'unixdt' and not both in same request. unixdt should be between today and next 14 day in Unix format. e.g. 1490227200
-  'hour': 56, // Number | Must be in 24 hour. For example 5 pm should be hour=17, 6 am as hour=6
-  'lang': "lang_example" // String | Returns 'condition:text' field in API in the desired language.<br /> Visit [request parameter section](https://www.weatherapi.com/docs/#intro-request) to check 'lang-code'.
-};
-
-var callback = function(error, data, response) {
-  if (error) {
-    console.error(error);
-  } else {
-    console.log('API called successfully. Returned data: ' + data.json());
+async function getApiKey(filePath) {
+  try {
+    const data = await readFile(filePath, 'utf8');
+    return data.trim(); // Trim any potential whitespace
+  } catch (error) {
+    console.error('Error reading file:', error);
+    throw error; // Rethrow the error to handle it later
   }
-};
-var forecast = apiInstance.forecastWeather(q, days, opts, callback);
+}
+
+(async () => {
+  try {
+    const apiKey = await getApiKey('../api_key.txt');
+    const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=Innisfil&days=1&aqi=no&alerts=no`;
+    console.log(apiUrl);
+  } catch (error) {
+    // Handle any errors that occurred during API key retrieval
+    console.error('Error:', error);
+  }
+})();
+
+
+const apiUrl = "https://api.weatherapi.com/v1/forecast.json?key="+get_api_key(filePath)+"&q=Innisfil&days=1&aqi=no&alerts=no"
+console.log(apiUrl)
+
+
+// Make the API call using fetch
+fetch(apiUrl)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`Network response was not ok: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then(data => {
+    // Process the weather data
+    console.log(data);
+  })
+  .catch(error => {
+    console.error('Error fetching weather data:', error);
+  });
